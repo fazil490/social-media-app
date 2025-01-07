6f112700330367a998f656ee31ce8d3158fa0a3c
Fazil <mdif4900@gmail.com>
2025-01-07 22:24:43 +0530

    login

---
+++ src/pages/Login.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import logo from "../assets/app_logo.png";
+
+import bg from "../assets/splash-screen-bg.png";
+import GoogleSignIn from "../components/GoogleSignIn";
+
+const Login: React.FC = () => {
+  return (
+    <section className="w-full h-full relative">
+      <img src={bg} className="h-[85vh] w-full rounded-t-xl" alt="background" />
+      <div className="rounded-b-xl bg-white h-[35%] md:h-[30%] xl:h-[35%]  absolute bottom-0 left-0 right-0 flex flex-col items-center justify-center px-3 py-10 rounded-t-[63px] shadow-2xl">
+        <div className="flex flex-col items-center gap-2 mb-7">
+          <div className="flex items-center gap-2">
+            <img src={logo} alt="app-logo" />
+            <p className="font-Karla text-[24px] md:text-[28px] font-semibold">
+              Feedly
+            </p>
+          </div>
+          <p className="font-KSans md:text-lg">
+            Moments That Matter, Shared Forever.
+          </p>
+        </div>
+        <GoogleSignIn />
+      </div>
+    </section>
+  );
+};
+
+export default Login;
