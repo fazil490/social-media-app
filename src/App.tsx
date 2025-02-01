720228df90463ae97bea05d064c04dad26f91d21
Fazil <mdif4900@gmail.com>
2025-02-01 20:31:03 +0530

    redux state persisted

--- src/App.tsx
+++ src/App.tsx
@@ -2,38 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import { Provider, useDispatch } from "react-redux";
-import store from "./redux/store";
-import { useEffect } from "react";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "./utils/firebase";
-import { logIn, logOut } from "./redux/userSlice";
+import store, { persistor } from "./redux/store";
 import NewPost from "./pages/NewPost";
 import { HomeProvider } from "./context/HomeContext";
 import Profile from "./pages/Profile";
+import { PersistGate } from "redux-persist/es/integration/react";
 // import ProtectedRoute from "./utils/ProtectedRoute";
 
 const AppContent = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, async (user) => {
-      if (user) {
-        const userData = {
-          uid: user.uid,
-          name: user.displayName,
-          email: user.email,
-          photoUrl: user.photoURL,
-          bio: null, // Update this as needed
-        };
-        dispatch(logIn(userData));
-      } else {
-        dispatch(logOut());
-      }
-    });
-
-    return () => unsubscribe(); // Cleanup the listener
-  }, [dispatch]);
-
   return (
     <div className="flex items-center justify-center h-[100vh]">
       <main className="w-full h-full lg:rounded-xl lg:h-[95vh] lg:my-auto lg:w-3/4 xl:w-1/2 mx-auto bg-zinc-50 lg:m-4">
@@ -60,7 +36,9 @@ const AppContent = () => {
 const App = () => {
   return (
     <Provider store={store}>
-      <AppContent />
+      <PersistGate loading={null} persistor={persistor}>
+        <AppContent />
+      </PersistGate>
     </Provider>
   );
 };
