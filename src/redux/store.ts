ed67ce458152f37a15ff31cca2b8272e27e2daa7
Fazil <mdif4900@gmail.com>
2025-02-01 20:26:10 +0530

    redux state persisted

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,29 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice"
+import postReducer from "./postSlice"
+import { PersistConfig } from "redux-persist";
+import storage from "redux-persist/lib/storage"
+import persistReducer from "redux-persist/es/persistReducer";
+import persistStore from "redux-persist/es/persistStore";
+
+const persistConfig: PersistConfig<RootState> = {
+  key: "root",
+  storage,
+}
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  post: postReducer
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 const store = configureStore({
-  reducer: {
-    user: userReducer
-  },
+  reducer: persistedReducer,
 });
 
-export type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof rootReducer>;
 export type AppDispatch = typeof store.dispatch;
 
+export const persistor = persistStore(store)
 export default store;
