022e7c1af938b91ba9d0f378ddef48e66298515c
Fazil <mdif4900@gmail.com>
2025-02-01 20:33:58 +0530

    post component

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,58 +1,88 @@
 import { FaRegHeart } from "react-icons/fa";
-import img1 from "../assets/splash-screen-bg.png";
-import img2 from "../assets/splash-screen-bg2.jpg";
 import { RiSendPlaneFill } from "react-icons/ri";
 import { useHomeContext } from "../context/HomeContext";
-const Post = () => {
-  const { openShare } = useHomeContext();
+import { PostType } from "./Feed";
+import { useState } from "react";
+import ImagePreview from "./ImagePreview";
+
+interface PostProps {
+  post: PostType;
+}
+
+const Post: React.FC<PostProps> = ({ post }) => {
+  const [showPreview, setShowPreview] = useState<string>("");
+  const {
+    description,
+    media,
+    createdAt,
+    likes,
+    userName,
+    userProfilePicture,
+    mediaType,
+  } = post;
+  const { openShare, setPostUrl } = useHomeContext();
+  // const handleShareClick = (url: string) => {
+  //   setPostUrl(`https://yourwebsite.com/posts/${url}`);
+  //   openShare();
+  // };
   return (
     <section className="rounded-3xl bg-postBgLav p-4 flex flex-col items-start gap-3">
       <div className="flex items-center gap-2">
         <img
-          className="w-10 h-10 rounded-full cursor-pointer"
-          src="https://lh3.googleusercontent.com/a/ACg8ocK1zvELb8eOVIWcahi0YB19JkGygQMfaPq5F-gqS0t7Zb2Nu-I=s96-c"
+          className="w-10 h-10 rounded-full cursor-pointer object-cover"
+          src={userProfilePicture}
           alt="user-profile-picture"
         />
         <div className="">
-          <p className="font-semibold text-base md:text-lg font-Karla">Fazil</p>
+          <p className="font-semibold text-base md:text-lg font-Karla">
+            {userName}
+          </p>
           <p className="text-[10px] md:text-xs text-black text-opacity-50 font-KSans">
             2 hours ago
           </p>
         </div>
       </div>
       <div>
-        <p className="text-xs md:text-sm font-KSans">
-          Just arrived in New York City! Excited to explore the sights, sounds,
-          and energy of this amazing place. 🗽 #NYC #Travel
-        </p>
+        <p className="text-xs md:text-sm font-KSans">{description}</p>
       </div>
       <div className="w-full h-[240px] flex gap-2 overflow-x-auto hide-scrollbar">
-        <img
-          className="w-[270px] object-cover rounded-xl"
-          src={img1}
-          alt="image1"
-        />
-        <img
-          className="w-[270px] object-cover rounded-xl"
-          src={img2}
-          alt="image2"
-        />
-        <img
-          className="w-[270px] object-cover rounded-xl"
-          src={img1}
-          alt="image1"
-        />
+        {media && media?.length > 0 && (
+          <>
+            {mediaType === "Photos" ? (
+              media?.map((media, i) => (
+                <img
+                  onClick={() => media && setShowPreview(media)}
+                  key={i}
+                  className="w-[270px] object-cover rounded-xl"
+                  src={media}
+                  alt={`media${i}`}
+                />
+              ))
+            ) : mediaType === "Video" ? (
+              <video
+                src={media[0]}
+                className="w-full h-full rounded-lg object-contain"
+                controls
+                autoPlay
+              />
+            ) : null}
+          </>
+        )}
       </div>
       <div className="flex items-center justify-between w-full">
         <span className="text-likePink flex items-center text-sm gap-1 font-medium cursor-pointer">
           <FaRegHeart className="text-lg" />
-          65
+          {likes}
         </span>
-        <div onClick={openShare} className="bg-black bg-opacity-[0.07] cursor-pointer px-2 py-1 flex items-center gap-1 rounded-3xl">
+        <div
+          onClick={openShare}
+          className="bg-black bg-opacity-[0.07] cursor-pointer px-2 py-1 flex items-center gap-1 rounded-3xl"
+        >
           <RiSendPlaneFill className="text-xl rounded-full" />
           <span className="text-sm font-Karla font-semibold">Share</span>
         </div>
       </div>
+      <ImagePreview url={showPreview} setShowPreview={setShowPreview} />
     </section>
   );
 };
